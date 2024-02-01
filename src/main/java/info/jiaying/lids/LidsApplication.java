6d6bb6989c0f4df7f0e98025c98d263864844593
Jiaying Huang <3395592153@qq.com>
2024-02-01 15:47:50 +0000

    Setup

---
+++ src/main/java/info/jiaying/lids/LidsApplication.java
@@ -0,0 +1,13 @@
+package info.jiaying.lids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LidsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/jiaying/lids/LidsApplicationTests.java
@@ -0,0 +1,13 @@
+package info.jiaying.lids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LidsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
